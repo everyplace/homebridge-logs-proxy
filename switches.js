65ea30b93975f0669637058452b8b6b291e15fb9
root <root@OpenWrt.lan>
2022-05-06 14:21:08 -0400

    initial mapping of switch ids to light ids

---
+++ switches.js
@@ -0,0 +1,10 @@
+const switches = {
+  32: [1,2,11,12,13,14], //kitchen
+  45: [15,18,20], //living room
+  44: [23], //oficina lamp
+   2: [9,10], //oficina ceiling fan
+  18: [16], //bedroom
+  19: [22] //quiet light
+}
+
+export default switches
