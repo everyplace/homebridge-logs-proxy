4373ac23da9d9b62f4fb1656dd539eeaf415953a
root <root@OpenWrt.lan>
2022-05-06 14:20:48 -0400

    Switches\!

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 import { Tail } from 'tail'
 import fetch from 'node-fetch'
-import { resolve } from "path"
-import { config } from "dotenv"
+import { resolve } from 'path'
+import { config } from 'dotenv'
+
+import switches from './switches.js'
                                                   
 //load .env
 import { fileURLToPath } from 'url'
@@ -9,7 +11,6 @@ import { dirname } from 'path'
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
 config({ path: resolve(__dirname, ".env") })
-
 
 const tail = new Tail("../homebridge.log")
 const { room, api } = process.env
@@ -22,6 +23,9 @@ tail.on("line", async function(data) {
     const raw = data.split('~DEVICE,')[1].split(',')
     console.log("Parseable data: ", data);
 
+
+
+    //device 2 = pico office
     const labels = {2:'on',5:'up',3:'fav',6:'down',4:'off'}
 
     const event = {
@@ -30,23 +34,26 @@ tail.on("line", async function(data) {
       action: raw[2]
     }
 
+    const lights = switches[event.remote] || [16]
+
     if(event.action == 4) return //button up
 
     console.log(event)
 
-    //button mapping to settings
-    if(['on','off'].includes(event.button.name)) {
-      console.log("on off action")
-      const url = `http://${api}/lights/${room}/${event.button.name}/23`
-      const response = await fetch(url).then(r=>r.json())
-      console.log(response)
-    } else {
-	const value = event.button.name === 'up' ? 200 : event.button.name === 'down' ? 50 : 125
-	const light = 23
-	const url = `http://${api}/lights/${room}/brightness/23/${value}`
-	console.log(url)
-	const response = await fetch(url).then(r=>r.json())
-	console.log({response})
+    for(let light of lights) {
+	    //button mapping to settings
+	    if(['on','off'].includes(event.button.name)) {
+            console.log("on off action")
+            const url = `http://${api}/lights/${room}/${event.button.name}/${light}`
+            const response = await fetch(url).then(r=>r.json())
+            console.log(response)
+	    } else {
+    		const value = event.button.name === 'up' ? 200 : event.button.name === 'down' ? 50 : 125
+    		const url = `http://${api}/lights/${room}/brightness/${light}/${value}`
+    		console.log(url)
+    		const response = await fetch(url).then(r=>r.json())
+    		console.log({response})
+	    }
     }
   } catch(e) {
     console.log("no valid action", data)
