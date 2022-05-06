86a3c5af532d903783e2df53ee4009c2d04757b3
root <root@OpenWrt.lan>
2022-05-06 17:24:43 -0400

    Added the dining room

--- switches.js
+++ switches.js
@@ -4,7 +4,8 @@ const switches = {
   44: [23], //oficina lamp
    2: [9,10], //oficina ceiling fan
   18: [16], //bedroom
-  19: [22] //quiet light
+  19: [22], //quiet light
+  34: [3,4,5,6] //dining room
 }
 
 export default switches
